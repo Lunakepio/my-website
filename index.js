00040f0aa0459c776f8e08d255738e109b19207b
Chelsea  Cadhila <chelsea.cadhila@eecs.fr>
2024-06-06 11:32:29 +0200

    Added fruits

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hello world')
+console.log('Hello world');
+
+const fruits = ['fraise', 'kiwi', 'mandarine']
+
+for(i = 0; i < fruits.length; i += 1){
+    console.log(`${fruits[i]}, c'est bon.`);
+}
